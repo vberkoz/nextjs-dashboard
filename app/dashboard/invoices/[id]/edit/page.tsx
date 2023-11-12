4b2d8fb893c343179121dddae9829dbb0fa5e0dd
vberkoz <vberkoz@gmail.com>
2023-11-13 01:52:21 +0200

    Chapter 13 Handling Errors

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,7 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { InvoiceForm } from '@/app/lib/definitions';
+import { notFound } from 'next/navigation';
  
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
@@ -19,6 +20,8 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   if (invoice) {
     invoiceData = invoice;
+  } else {
+    notFound();
   }
 
   return (
